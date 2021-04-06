5ef77a2a5a24d460346c14cc4dc2630b0509f672
Mate Polocz <48856366+poloczmate@users.noreply.github.com>
2021-04-06 16:21:16 +0200

    added link to github repo

--- src/main/java/MCP.java
+++ src/main/java/MCP.java
@@ -1,5 +1,7 @@
 import trafficlight.ctrl.TrafficLightCtrl;
 
+//TODO Github Repo
+//github repository: https://github.com/poloczmate/trafficlight2021
 
 public class MCP {
     public static void main(String[] args) {
